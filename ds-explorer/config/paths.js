06d650a6e057df147f078621ca9128a39b34c61b
Chris Laplante <chris.laplante@agilent.com>
2024-02-12 20:51:54 -0500

    ugh

--- ds-explorer/config/paths.js
+++ ds-explorer/config/paths.js
@@ -15,7 +15,7 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 // single-page apps that may serve index.html for nested URLs like /todos/42.
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
-const publicUrlOrPath = getPublicUrlOrPath(require(resolveApp('package.json')).homepage);
+const publicUrlOrPath = require(resolveApp('package.json')).homepage;
 
 const buildPath = process.env.BUILD_PATH || 'build';
 
